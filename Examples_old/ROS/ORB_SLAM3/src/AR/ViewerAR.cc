aa9bc0e50af2f29d243e31a8e257d2bb692a6e0e
Pyrokine <pyrokinetech@gmail.com>
2022-12-23 10:13:17 -0500

    Support OpenCV4 and C++17

--- Examples_old/ROS/ORB_SLAM3/src/AR/ViewerAR.cc
+++ Examples_old/ROS/ORB_SLAM3/src/AR/ViewerAR.cc
@@ -19,6 +19,7 @@
 #include "ViewerAR.h"
 
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/eigen.hpp>
 
 #include <mutex>
 #include <thread>
@@ -402,7 +403,9 @@ Plane* ViewerAR::DetectPlane(const cv::Mat Tcw, const std::vector<MapPoint*> &vM
         {
             if(pMP->Observations()>5)
             {
-                vPoints.push_back(pMP->GetWorldPos());
+                cv::Mat WorldPos;
+                cv::eigen2cv(pMP->GetWorldPos(), WorldPos);
+                vPoints.push_back(WorldPos);
                 vPointMP.push_back(pMP);
             }
         }
@@ -527,7 +530,8 @@ void Plane::Recompute()
         MapPoint* pMP = mvMPs[i];
         if(!pMP->isBad())
         {
-            cv::Mat Xw = pMP->GetWorldPos();
+            cv::Mat Xw;
+            cv::eigen2cv(pMP->GetWorldPos(), Xw);
             o+=Xw;
             A.row(nPoints).colRange(0,3) = Xw.t();
             nPoints++;
